7f8e8b5d69e900913a9eb82affd931d3551b337f
UchiTesing <uchitesting57@gmail.com>
2022-06-01 14:31:11 +0200

    Added a few Use middlewares to observe their behaviour

--- Program.cs
+++ Program.cs
@@ -1,7 +1,39 @@
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World Map!");
+
+app.Use(async (httpContext, nextMiddleware) =>
+{
+    Console.WriteLine($"\n\n\n======== {DateTime.Now.ToString("f", CultureInfo.InvariantCulture)} ========");
+    await nextMiddleware();
+});
+
+app.Use(async (httpContext, nextMiddleware) =>
+{
+    Console.WriteLine("Before Request 1");
+    await httpContext.Response.WriteAsync("Use 1.\n");
+    await nextMiddleware();
+    Console.WriteLine("After Request 1");
+});
+
+app.Use(async (httpContext, nextMiddleware) =>
+{
+    Console.WriteLine("Before Request 2");
+    await httpContext.Response.WriteAsync("Use 2.\n");
+    await nextMiddleware();
+    Console.WriteLine("After Request 2");
+});
+
+app.Use(async (httpContext, nextMiddleware) =>
+{
+    Console.WriteLine("Before Request 3");
+    await httpContext.Response.WriteAsync("Use 3.\n");
+    await nextMiddleware();
+    Console.WriteLine("After Request 3");
+});
 
 app.Run(async context => await context.Response.WriteAsync("Hello World!"));
 app.Run(); // Application won't display without that final empty Run().
