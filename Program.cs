2eb8eb4b4d207f15378553453d637ccc6c9cb721
UchiTesing <uchitesting57@gmail.com>
2022-06-01 13:35:41 +0200

    Replaced code with minimal terminal delegate

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World Map!");
 
-app.Run();
+app.Run(async context => await context.Response.WriteAsync("Hello World!"));
+app.Run(); // Application won't display without that final empty Run().
